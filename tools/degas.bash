90d5edcdbdb03b562f2cbf4f8f9f33f0480f0b84
David Morrin <dwmorrin@gmail.com>
2019-04-07 00:03:26 -0400

    fix: auto suggest changes
    merges chext, {add,remove}tags into {de,re}gas.
    loses hardcoding for js, will look for js and css now
    
    fixes issue #1

--- tools/degas.bash
+++ tools/degas.bash
@@ -1,38 +1,74 @@
 #!/bin/bash
-# requires chext.sh and removetags.sh
 # helper script for working on Google Apps Script projects with clasp
+# converts .html files starting with <script> into .js files
+# converts .css files starting with <style> into .css files
+
 progname="$(basename "$0")"
+
 # check for presence of appropriate files first
 if ! compgen -G "*.html" >/dev/null; then
-    echo "$progname chexts all .html to .js: no .html found"
+    echo "$progname converts .html to .js or .css: no .html found"
     exit 1
 fi
-# without arguments: process all matching files in pwd
-if [ "$#" -eq 0 ]; then
-    read -r -p "Convert all .html to .js and remove <script> tags? [y/n]" confirmation
-    if [ "$confirmation" != "y" ]; then
-        exit 1
-    fi
 
-    chext html js
-    for file in *.js; do
-        removetags script "$file"
-    done
-
-    exit 0
-fi
-# with arguments: process only the listed files
-for file; do
-    if [ "${file##*.}" != html ]; then
-        echo "$file is not .html, cannot covert with $progname, aborting"
-        exit 1
-    fi
-    read -r -p "Convert $file to .js and remove <script> tags? [y/n]" confirmation
-    if [ "$confirmation" != "y" ]; then
-        exit 1
-    fi
-
-    chext html js "$file"
-    removetags script "${file%html}js"
+quiet=false
+while getopts "q" opt; do
+    case "$opt" in
+        q) quiet=true;;
+       \?) echo "Usage: $progname [-q]"
+           exit 1;;
+    esac
 done
+shift $((OPTIND -1))
+
+# note: compgen needs nullglob unset
+shopt -s nullglob
+
+noargs=false
+if [[ $# -eq 0 ]]; then
+    noargs=true
+    # insert all .html files in pwd as arguments
+    set -- *.html
+fi
+
+if ! $quiet && $noargs; then
+    read -r -p "Convert all .html to .js or .css and remove html tags? [y/n] " confirmation
+    if [ "$confirmation" != "y" ]; then
+        exit 1
+    fi
+fi
+
+scriptPattern='<script>'
+stylePattern='<style>'
+for file; do
+    unset ext
+    firstline=$(sed 1q "$file")
+    if [[ $firstline =~ $scriptPattern ]]; then
+        ext="js"
+        tag="script"
+    elif [[ $firstline =~ $stylePattern ]]; then
+        ext="css"
+        tag="style"
+    fi
+    if [[ -v ext ]]; then
+        if ! $quiet; then
+            read -r -p "Convert $file to $ext? [y/n] " confirmation
+            if [ "$confirmation" != "y" ]; then
+                continue
+            fi
+            echo "  Converting $file"
+        fi
+        if sed -i'.bak' '
+            /<\/*'"$tag"'>/d' "$file"; then
+            rm "$file.bak"
+        else
+            echo "aborting: sed exited with error. check for a .bak file."
+            exit 1
+        fi
+        mv "$file" "${file%html}$ext"
+    elif ! $quiet; then
+        echo "  Ignoring $file: does not appear to be a js or css file"
+    fi
+done
+
 exit 0
--- tools/regas.bash
+++ tools/regas.bash
@@ -1,42 +1,81 @@
 #!/bin/bash
-# requires chext.sh and addtags.sh
 # helper script for working on Google Apps Script projects with clasp
+
 progname="$(basename "$0")"
+
 # check for presence of appropriate files first
-if ! compgen -G "*.js" >/dev/null; then
-    echo "$(basename "$0") chexts all .js to .html: no .js found"
+if ! compgen -G "*."{js,css} >/dev/null; then
+    echo "$(basename "$0") converts .js and .css to .html: no .js or .css found"
     exit 1
 fi
 
-# without arguments: process all matching files in pwd
-if [ "$#" -eq 0 ]; then
+quiet=false
+while getopts "q" opt; do
+    case "$opt" in
+        q) quiet=true;;
+       \?) echo "Usage: $progname [-q]"
+           exit 1;;
+    esac
+done
+shift $((OPTIND -1))
+
+# note: compgen needs nullglob unset
+shopt -s nullglob
+
+noargs=false
+if [[ $# -eq 0 ]]; then
+    noargs=true
+    # insert all .js and .css files in pwd as arguments, requires nullglob
+    set -- *.{js,css}
+fi
+
+if ! $quiet && $noargs; then
     read -r -p "Convert all .js to .html and add <script> tags? [y/n]" confirmation
     if [ "$confirmation" != "y" ]; then
         exit 1
     fi
-    
-    chext js html 
-    for file in *.html; do
-        addtags script "$file"
-    done
-
-    exit 0
 fi
-
-# with arguments: process only the listed files
+    
 for file; do
-    if [ "${file##*.}" != js ]; then
-        echo "$file is not .js, cannot covert with $progname, aborting"
+    ext="${file##*.}"
+    case "$ext" in
+        "js") tag="script";;
+        "css") tag="style";;
+        *)
+        if ! $quiet; then
+            echo "  Ignoring $file: does not appear to be js or css file"
+        fi
+        continue;;
+    esac
+    firstline=$(sed 1q "$file")
+    if [[ $firstline =~ $tag ]]; then
+        if ! $quiet; then
+            echo "  Ignoring $file: already has $tag tag in first line"
+        fi
+        continue
+    fi
+    if ! $quiet; then
+        read -r -p "Convert $file to .html with $tag tags? [y/n] " confirmation
+        if [ "$confirmation" != "y" ]; then
+            continue
+        fi
+    fi
+    # shellcheck disable=SC1004
+    if sed -i'.bak' '
+1i\
+<'"$tag"'>
+
+$a\
+</'"$tag"'>
+' "$file"
+    then
+        rm "$file.bak"
+    else
+        echo "aborting: sed exited with error. Check for a .bak file."
         exit 1
     fi
-    read -r -p "Convert $file to .html and add <script> tags? [y/n]" confirmation
-    if [ "$confirmation" != "y" ]; then
-        exit 1
-    fi
+    mv "$file" "${file%$ext}html"
 
-    chext js html "$file"
-    addtags script "${file%js}html"
-
-    shift
 done
+
 exit 0
